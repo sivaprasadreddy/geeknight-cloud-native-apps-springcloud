e55ab81a030f155e4da85c505346a3e3d4061597
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-10-29 13:58:25 +0530

    Adding local vuejs, jquery.

---
+++ catalog-service/src/main/java/com/geeknight/catalogservice/FeignBeanFactoryPostProcessor.java
@@ -0,0 +1,18 @@
+package com.geeknight.catalogservice;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+//https://github.com/spring-cloud/spring-cloud-netflix/issues/1952
+@Component
+public class FeignBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition bd = beanFactory.getBeanDefinition("feignContext");
+        bd.setDependsOn("eurekaServiceRegistry", "inetUtils");
+    }
+}
